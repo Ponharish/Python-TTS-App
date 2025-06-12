de70602e0d9f3510315b5e452d2c50293f6c4b10
Ponharish <e1123017@u.nus.edu>
2025-06-12 20:56:34 +0800

    Add PoC code

---
+++ Proof_of_Concept.py
@@ -0,0 +1,35 @@
+import boto3
+import pygame
+import io
+
+f = open('.secret_keys.txt', 'r')
+keys = f.read()
+keys = keys.split(',')
+aws_access_key = keys[0].strip()
+aws_secret_key = keys[1].strip()
+region_name = 'ap-southeast-1'
+
+text = "Hello World. This is amazon polly test. This test is done by Harish"
+
+polly_client = boto3.client(
+    'polly',
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key,
+    region_name=region_name
+)
+    
+response = polly_client.synthesize_speech(
+    Text=text,
+    OutputFormat='mp3',
+    VoiceId='Joanna'
+)
+
+audio_stream = response['AudioStream']
+
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load(io.BytesIO(audio_stream.read()))
+pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
